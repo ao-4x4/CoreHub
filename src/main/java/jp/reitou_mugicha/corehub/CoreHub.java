c8ded1f89d7d6aa5dcc69cc06d71f0b248353f9a
ao-4x4 <kounoaoto1029@icloud.com>
2025-01-26 13:43:39 +0900

    簡略化

--- src/main/java/jp/reitou_mugicha/corehub/CoreHub.java
+++ src/main/java/jp/reitou_mugicha/corehub/CoreHub.java
@@ -4,12 +4,10 @@ import jp.reitou_mugicha.corehub.craft.Test;
 import jp.reitou_mugicha.corehub.extended_enderchest.EnderchestDataManager;
 import jp.reitou_mugicha.corehub.extended_enderchest.EnderchestUpgrade;
 import jp.reitou_mugicha.corehub.extended_enderchest.ExtendedEnderchest;
-import jp.reitou_mugicha.corehub.feature.BulkTrading;
-import jp.reitou_mugicha.corehub.feature.ExperienceTrading;
-import jp.reitou_mugicha.corehub.feature.InstantChest;
-import jp.reitou_mugicha.corehub.feature.UnlimitedAnvil;
+import jp.reitou_mugicha.corehub.feature.*;
 import jp.reitou_mugicha.corehub.fix.FixAnvilSweepingEdge;
 import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.mineacademy.fo.plugin.SimplePlugin;
 
 public final class CoreHub extends SimplePlugin
@@ -22,16 +20,21 @@ public final class CoreHub extends SimplePlugin
         enderchestDataManager = new EnderchestDataManager(getDataFolder());
 
         // EVENT REGISTER
-        getServer().getPluginManager().registerEvents(new FixAnvilSweepingEdge(), this);
-        getServer().getPluginManager().registerEvents(new InstantChest(), this);
-        getServer().getPluginManager().registerEvents(new BulkTrading(), this);
-        getServer().getPluginManager().registerEvents(new ExperienceTrading(), this);
-        getServer().getPluginManager().registerEvents(new UnlimitedAnvil(), this);
-        getServer().getPluginManager().registerEvents(new ExtendedEnderchest(enderchestDataManager), this);
-        getServer().getPluginManager().registerEvents(new EnderchestUpgrade(enderchestDataManager), this);
+        event(new FixAnvilSweepingEdge());
+        event(new InstantChest());
+        event(new BulkTrading());
+        event(new ExperienceTrading());
+        event(new UnlimitedAnvil());
+        event(new ExtendedEnderchest(enderchestDataManager));
+        event(new EnderchestUpgrade(enderchestDataManager));
 
         Test.register();
         Bukkit.getLogger().info("CoreHub is Enabled.");
+    }
+
+    private void event(Listener listener)
+    {
+        getServer().getPluginManager().registerEvents(listener, this);
     }
 
     @Override
